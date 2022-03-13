eb31260cd2e005f4cad8ba616964c1a0ce32712b
pc <pc@DESKTOP-GQKSQ03>
2022-03-13 10:01:29 -0700

    my first commit to local repository

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class Test1
+{
+
+	public static void main(String[] args)
+	{
+		String x="ffsskfn7586743uhk654ght$%#@^";
+		String y=x.replaceAll("[^0-9]","");
+		System.out.println(y);
+
+	}
+
+}
