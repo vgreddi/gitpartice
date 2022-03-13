d07c6657c006cd3ace3f3e2006f9edd086365a95
vgreddi <101520748+vgreddi@users.noreply.github.com>
2022-03-13 11:07:17 -0700

    Update Test1.java
    changed

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -5,7 +5,7 @@ public class Test1
 
 	public static void main(String[] args)
 	{
-		String x="ffsskfn7586743uhk654ght$%#@^";
+		String x="ffsskfn7586743uhk654ght$%#@^"venu;
 		String y=x.replaceAll("[^0-9]","");
 		System.out.println(y);
 
